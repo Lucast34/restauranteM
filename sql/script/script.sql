82f070f72ffecc69cb5eb06c5143ff7e6dfdb726
Lucast34 <lucast34t@gmail.com>
2025-02-14 21:45:18 -0300

    insert

--- sql/script/script.sql
+++ sql/script/script.sql
@@ -1,11 +1,9 @@
-show tables;
-
 create table Comanda(
 	numeroComanda serial primary key,
 	mesa int,
 	nome varchar(250),
-	dt_hrAbertura date,
-	dt_hrFechamento date
+	dt_hrAbertura timestamp,
+	dt_hrFechamento timestamp
 	
 );
 
@@ -13,7 +11,7 @@ select * from Comanda;
 
 create table Pedido(
 	id_pedido serial primary key,
-	pedido_dt_hr date,
+	pedido_dt_hr timestamp,
 	pratos int,
 	id_comandaPedido int,
 	id_cardapaioPedido int,
@@ -27,22 +25,16 @@ select * from Pedido;
 create table Cardapio (
     id_cardapio serial primary key,
     nome varchar(250),
-<<<<<<< HEAD
     descricao text,
-=======
->>>>>>> 30bcff4d8863a4a3c5fddd5bddd07440ca6e2c30
     preco float,
     estoque int
 );
 
-<<<<<<< HEAD
 select  * from cardapio c ;
 
-drop table comanda;
+drop table comanda cascade;
 drop table pedido;
 drop table cardapio cascade;
 
 select * from Cardapio c;
-=======
 select * from Cardapio c;
->>>>>>> 30bcff4d8863a4a3c5fddd5bddd07440ca6e2c30
